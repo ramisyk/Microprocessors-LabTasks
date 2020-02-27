054ebc02346e772696f9f873c1b16cdd3d957ebf
ramisyk <45203859+ramisyk@users.noreply.github.com>
2020-02-27 15:07:06 +0300

    Update Loop-Mul-Div.asm

--- Loop-Mul-Div.asm
+++ Loop-Mul-Div.asm
@@ -3,16 +3,17 @@ org 100h
 ; add your code here  
 
 
-MOV CX, 10
-MOV AX, 16
-MOV BX, 02 
+MOV CX, 10            ;CX = 10 for loop counter
+MOV AX, 16            ;AX = 16 variable
+MOV BX, 02            ;BX = 2  varianle
 
-MUL_LOOP:  MUL BX 
-           DEC CX
-           JNZ MUL_LOOP   
-MOV CX, 7          
-MOV SI, AX
-DIV CX
+MUL_LOOP:  MUL BX                ;AX = AX * BX with flags
+           DEC CX                ;CX = CX - 1 for loop control
+           JNZ MUL_LOOP          ;if CX = 0 jump to MUL_LOOP
+           
+MOV CX, 7             ;CX = 7 variable
+MOV SI, AX            ;SI = AX  for divison, 
+DIV CX                ;SI = SI/CX
   
 
 ret
